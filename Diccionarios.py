fc1e90deef67806929593487320c4ecaca2f37fc
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-28 11:20:17 -0600

    primera version

---
+++ Diccionarios.py
@@ -0,0 +1,19 @@
+#Introducción a Diccionarios
+
+diccionario = {"azul":"blue","rojo":"red","amarillo":"yellow","verde":"green"} #Primero se anota la clave y después el  valor
+diccionario2 ={"Carolina":[20,1.74],"Luis":[19,1.75]}
+
+
+#Para buscar un valor
+print(diccionario["azul"])
+
+#Agregar valores
+diccionario["Morado"] = "purple"
+
+#Para eliminar un valor
+del(diccionario["verde"])
+
+print(diccionario)
+print(diccionario2["Carolina"])
+
+#Carolina EM
